fe7f2124ce06e11117ade7866512efab58ed2270
Rowan Mackenzie <row.mackenzie@gmail.com>
2023-02-20 16:44:45 -0800

    added an EJS index file, passed exisiting shortened URLs to it

--- express_server.js
+++ express_server.js
@@ -16,9 +16,16 @@ app.get("/", (req, res) => {
   res.send("<html><body>Hello! This is your <b>home page</b>!</body></html>\n");
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+app.get("/urls", (req, res) => {
+const templateVars = {
+  urls : urlDatabase
+}
+  res.render("urls_index", templateVars);
 });
+
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
